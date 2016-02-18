1ea510bd2eb57f0adb9ef2d061e1f705f32039f0
Vidya Raghavan <vidya@shippable.com>
2016-02-18 13:16:07 +0530

    Update test.js

--- test.js
+++ test.js
@@ -3,12 +3,13 @@ var superagent = require("superagent"),
     expect = chai.expect,
     should = require("should");
 
+
 describe("Index", function () {
   it("renders HTML", function (done) {
     superagent.get("http://localhost:3000/")
       .end(function (e, res) {
         (e === null).should.equal(true);
-        res.text.should.equal("Hey buddy!");
+        res.text.should.equal("Hey budady!");
         done();
       });
   });
